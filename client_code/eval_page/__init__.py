6d6a3b1094aa21d9d0a1679e18314afaf81ff36d
jakeb3175@vt.edu <jakeb3175@vt.edu>
2026-03-24 19:30:02 +0000

    Created Form2, eval_page.html and eval_page; Edited settings; Deleted standard-page.html and Form1

--- client_code/eval_page/__init__.py
+++ client_code/eval_page/__init__.py
@@ -1,7 +1,7 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import eval_pageTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class eval_page(eval_pageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
