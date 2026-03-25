8ea52b0f64bde10e35fd3cce99d64d3610093e28
jakeb3175@vt.edu <jakeb3175@vt.edu>
2026-03-25 00:07:18 +0000

    Edited eval_form_final

--- client_code/eval_form_final/__init__.py
+++ client_code/eval_form_final/__init__.py
@@ -18,20 +18,19 @@ class eval_form_final(eval_form_finalTemplate):
 
     
 
-    rating_items = [
-      ("Select a rating...", None),
-      ("1. Poor", 1),
-      ("2. Acceptable", 2),
-      ("3. Good", 3),
-      ("4. Excellent", 4)
-    ]
+    contribute_items = [("Contributes to Team Project", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
+    facilitates_items = [("Facilitates Contributions of Others", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
+    planning_items = [("Plans & Manages Team Project Goals", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
+    fosters_items = [("Fosters a Team Climate", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
+    manages_items = [("Manages Potential Conflict", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
+    overall_items = [("Overall", None), ("0- Never", 0), ("1- Sometimes", 1), ("2 Usually", 2), ("3- Regularly", 3), ("4- Always", 4)]
 
-    self.contribute_dd.items = rating_items
-    self.facilitates_dd.items = rating_items
-    self.planning_dd.items = rating_items
-    self.fosters_dd.items = rating_items
-    self.manages_dd.items = rating_items
-    self.overall_dd.items = rating_items
+    self.contribute_dd.items = contribute_items
+    self.facilitates_dd.items = facilitates_items
+    self.planning_dd.items = planning_items
+    self.fosters_dd.items = fosters_items
+    self.manages_dd.items = manages_items
+    self.overall_dd.items = overall_items
 
     self.contribute_dd.selected_value = None
     self.facilitates_dd.selected_value = None
@@ -73,4 +72,27 @@ class eval_form_final(eval_form_finalTemplate):
     
       alert("Evaluation submitted!")
   pass
+
+  @handle("cancel_btn", "click")
+  def cancel_btn_click(self, **event_args):
+    dropdown = [
+      self.contribute_dd,
+      self.facilitates_dd,
+      self.planning_dd,
+      self.fosters_dd,
+      self.manages_dd,
+      self.overall_dd,
+      self.student_dropdown
+    ]
+
+    for dd in dropdown:
+        dd.selected_value = None
+        
+    
+  pass
+
+ 
+
+
+  
       
