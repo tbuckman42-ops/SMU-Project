c397c95b06f1400059df2ad0bd1bf8354cc62867
jakeb3175@vt.edu <jakeb3175@vt.edu>
2026-03-26 20:13:03 +0000

    Edited home_page and confirmation_page

--- client_code/confirmation_page/__init__.py
+++ client_code/confirmation_page/__init__.py
@@ -18,3 +18,13 @@ class confirmation_page(confirmation_pageTemplate):
   def home_btn_click(self, **event_args):
     open_form('home_page')
     pass
+
+  @handle("chart_btn", "click")
+  def chart_btn_click(self, **event_args):
+    open_form('charts')
+    pass
+
+  @handle("home_btn_2", "click")
+  def home_btn_2_click(self, **event_args):
+    open_form('home_page')
+    pass
